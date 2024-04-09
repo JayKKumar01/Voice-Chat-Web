18efabadfa4f6a35579c9880a141e6864c85d6b9
Jay Kumar <jaytechbc@gmail.com>
2024-04-09 12:40:14 +0530

    update

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const logsTextarea = document.getElementById('logs');
 const peerBranch = "JayKKumar01-Voice_Chat_Web-";
+const randomId = getRandomCode();
 
 let peer;
 let myId;
 let connections = [];
-let peers = [];
+let peers = {};
 let isHost = true;
+let myName;
+
+document.addEventListener('DOMContentLoaded', function () {
+    setRandomName();
+});
+
+
 
 
 function getRandomCode() {
@@ -18,15 +26,24 @@ function appendLog(log) {
     logsTextarea.scrollTop = logsTextarea.scrollHeight;
 }
 
+function setRandomName() {
+    const names = ["Tom Cruise", "Angelina Jolie", "Brad Pitt", "Jennifer Aniston", "Leonardo DiCaprio", "Scarlett Johansson", "Johnny Depp", "Jennifer Lawrence", "Will Smith", "Natalie Portman", "George Clooney", "Meryl Streep", "Robert Downey Jr.", "Emma Stone", "Matt Damon", "Julia Roberts", "Chris Hemsworth", "Sandra Bullock", "Dwayne Johnson", "Anne Hathaway"];
+    const randomIndex = Math.floor(Math.random() * names.length);
+    const randomName = names[randomIndex];
+    document.getElementById('nameField').value = randomName;
+}
+
+
 function openPeer() {
     const nameInput = document.getElementById('nameField').value;
     if (nameInput.length === 0) {
         appendLog('Please enter your Name...');
         return;
     }
+    myName = nameInput;
 
     // Create a PeerJS instance
-    const randomId = getRandomCode();
+
     const peerId = `${peerBranch}${randomId}`;
 
     peer = new Peer(peerId);
@@ -39,19 +56,19 @@ function openPeer() {
         }
     });
 
-    peer.on('connection', setupConnection);
+    peer.on('connection', function (connection) {
+        connection.on('open', () => setupConnection(connection));
+    });
 }
 
 function connectPeer() {
     const codeInput = document.getElementById('codeField').value;
-    const nameInput = document.getElementById('nameField').value;
     connect(codeInput);
 }
 
 function connect(otherId) {
     let connection = peer.connect(peerBranch + otherId, { reliable: true });
     connection.on('open', () => setupConnection(connection));
-    // connection.on('open', () => setupConnection(connection));
     // connection.on('close', onDataConnectionClose);
     // connection.on('error', onDataConnectionError);
 }
@@ -64,17 +81,22 @@ function setupConnection(connection) {
     connection.on('data', handleData);
     connection.on('error', (err) => appendLog(`Connection error: ${err}`));
 
+    connection.send({
+        type: 'askName',
+        peerId: randomId,
+        name: myName
+    });
+
     if (isHost) {
-        
-        connection.on('open', () => {
-            connection.send({
-                type: 'userlist',
-                data: peers
-            });
-            peers.push({id: remoteId, name: null});
+
+        connection.send({
+            type: 'userlist',
+            data: peers
         });
+        peers[remoteId] = peers[remoteId] || null;
 
     }
+
 }
 
 
@@ -84,9 +106,13 @@ function handleData(data) {
     switch (data.type) {
         case 'userlist':
             const userlist = data.data;
-            userlist.forEach(peer => {
-                connect(peer.id);
-            });
+            for (const remoteId in userlist) {
+                connect(remoteId);
+            }
+            break;
+        case 'askName':
+            peers[data.peerId] = data.name;
+            appendLog(data.peerId + ": " + data.name);
             break;
         default:
             // Handle other types of data if needed
